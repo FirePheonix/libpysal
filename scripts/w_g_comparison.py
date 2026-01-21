0703f2f788f7ad586e96b207d7dfbc0a05d328e1
FirePheonix <shubhsoch@gmail.com>
2026-01-21 08:40:43 +0530

    testing-scripts-addition

--- scripts/w_g_comparison.py
+++ scripts/w_g_comparison.py
@@ -244,5 +244,5 @@ g_content = [line.split(",") for line in g_content]
 content = f"{content}\n\n{create_rst_table(g_content)}"
 
 
-with open("../docs/migration.rst", "w") as guide:
+with open("../docs/migration.rst", 'w') as guide:
     guide.write(content)
